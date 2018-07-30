e77d4345ab4dc489202c421c4559f70ed46083cf
rbchan <richard.chandlers@gmail.com>
2018-07-30 11:21:32 -0400

    newcommand for inline R code

--- labs/intro-to-R/build.R
+++ labs/intro-to-R/build.R
@@ -1,13 +1,24 @@
 
-if(!require(knitr)) {
+## Load knitr or install if need be
+reqval <- require(knitr)
+if(!reqval) {
     install.packages("knitr")
+    library(knitr)
 }
 
+## Create the .tex file
 knit("lab-intro-to-R.Rnw")
-tools::texi2dvi("lab-intro-to-R.tex", pdf=TRUE,
-                clean=TRUE)
 
-if(system("open lab-intro-to-R.pdf") != 0) {
+
+## Create the PDF
+tools::texi2dvi("lab-intro-to-R.tex", pdf=TRUE, clean=TRUE)
+
+
+## Open the PDF
+## This will only work if Rcmd open is installed
+sysval <- system("open lab-intro-to-R.pdf")
+
+if(sysval != 0) {
     warning("\n\nThe PDF should be in your working directory")
 }
 
