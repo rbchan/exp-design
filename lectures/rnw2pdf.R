cad7e7f1edd8bb51749222667fd9d24e240d88a5
Richard Chandler <richard.chandlers@gmail.com>
2018-09-07 17:31:41 -0400

    power analysis lecture

---
+++ lectures/rnw2pdf.R
@@ -0,0 +1,33 @@
+rnw2pdf <- function(filestub, env=globalenv(), tangle=FALSE,
+                    open=TRUE, clean=TRUE, ...) {
+
+    ## Load knitr or install if need be
+    reqval <- require(knitr)
+    if(!reqval) {
+        install.packages("knitr")
+        library(knitr)
+    }
+
+    ## Create the .tex file
+    rnw.file <- paste(filestub, ".Rnw", sep="")
+    tex.file <- knit(rnw.file, envir=env, ...)
+
+    ## Create the .R file
+    if(tangle)
+        knit(rnw.file, tangle=TRUE)
+
+    ## Create the PDF
+    ##tools::texi2dvi("lab-intro-to-R.tex", pdf=TRUE)
+    tools::texi2dvi(tex.file, pdf=TRUE, clean=clean)
+
+    ## Open the PDF
+    ## This will only work if Rcmd open is installed
+    if(open) {
+        open.pdf <- paste("open ", filestub, ".pdf", sep="")
+        sysval <- system(open.pdf)
+        if(sysval != 0) {
+            warning("\n\nThe PDF should be in your working directory")
+        }
+    }
+
+}
