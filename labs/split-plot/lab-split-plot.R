9d6f0023bfde74b95c8b1aa8eb5320d9701a5355
rbchan <richard.chandlers@gmail.com>
2018-10-07 14:14:14 -0400

    split plot handout

--- labs/split-plot/lab-split-plot.R
+++ labs/split-plot/lab-split-plot.R
@@ -1,51 +1,43 @@
-## ----meatData,size='small'-----------------------------------------------
+## ----meatData,size='scriptsize'------------------------------------------
 meatData <- read.csv("meatData.csv")
 str(meatData)
 
-## ----factors,size='small'------------------------------------------------
+## ----factors,size='footnotesize'-----------------------------------------
 meatData$time <- factor(meatData$time)
 meatData$carcass <- factor(meatData$carcass)
 meatData$roast <- factor(meatData$roast)
 
-## ----aov1,size='small'---------------------------------------------------
+## ----aov1,size='footnotesize'--------------------------------------------
 aov.meat1 <- aov(Wbscore ~ tenderizer * time + carcass +
                  Error(roast), data=meatData)
 
-## ----saov1,size='small'--------------------------------------------------
+## ----saov1,size='scriptsize'---------------------------------------------
 summary(aov.meat1)
 
-## ----lme1,size='small'---------------------------------------------------
+## ----lme1,size='footnotesize'--------------------------------------------
 library(nlme)
-lme.meat1 <- lme(Wbscore ~ tenderizer*time,
-    data=meatData,
+lme.meat1 <- lme(Wbscore ~ tenderizer*time, data=meatData,
     correlation=corCompSymm(), # To make results same as aov()
     random = ~1|carcass/roast)
 
 
-## ----alme1,size='small'--------------------------------------------------
+## ----alme1,size='footnotesize'-------------------------------------------
 anova(lme.meat1)
 
-## ----lmeP,size='tiny'----------------------------------------------------
-lme.meatP <- lme(Wbscore ~ time, data=meatData,
-                 random = ~1|carcass/roast, correlation=corCompSymm(),
-                 subset=tenderizer=="P")
-
-## ----lmeV,size='tiny'----------------------------------------------------
-lme.meatV <- lme(Wbscore ~ time, data=meatData,
-                 random = ~1|carcass/roast,correlation=corCompSymm(),
-                 subset=tenderizer=="V")
-
-## ----lmeC,size='tiny'----------------------------------------------------
-lme.meatC <- lme(Wbscore ~ time, data=meatData,
-                 random = ~1|carcass/roast, correlation=corCompSymm(),
-                 subset=tenderizer=="C")
-
 ## ----lmePVC,size='tiny'--------------------------------------------------
+lme.meatP <- lme(Wbscore ~ time, data=meatData, random = ~1|carcass/roast,
+                 correlation=corCompSymm(), subset=tenderizer=="P")
+lme.meatV <- lme(Wbscore ~ time, data=meatData, random = ~1|carcass/roast,
+                 correlation=corCompSymm(), subset=tenderizer=="V")
+lme.meatC <- lme(Wbscore ~ time, data=meatData, random = ~1|carcass/roast,
+                 correlation=corCompSymm(), subset=tenderizer=="C")
+
+## ----almePVC,size='tiny'-------------------------------------------------
 anova(lme.meatP, Terms="time")
 anova(lme.meatV, Terms="time")
 anova(lme.meatC, Terms="time")
 
-## ----tuk,size='scriptsize'-----------------------------------------------
+## ----tuk,size='tiny'-----------------------------------------------------
 # install.packages("multcomp")
 library(multcomp)
 mcP <- glht(lme.meatP, linfct=mcp(time="Tukey"))
