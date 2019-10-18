4eef3ee6035338601528697d90fb03e29b0c63f2
rbchan <richard.chandlers@gmail.com>
2019-10-18 12:28:21 -0400

    Split-plot lab

--- labs/split-plot/lab-split-plot.R
+++ labs/split-plot/lab-split-plot.R
@@ -2,17 +2,21 @@
 meatData <- read.csv("meatData.csv")
 str(meatData)
 
+
 ## ----factors,size='footnotesize'-----------------------------------------
 meatData$time <- factor(meatData$time)
 meatData$carcass <- factor(meatData$carcass)
 meatData$roast <- factor(meatData$roast)
 
+
 ## ----aov1,size='footnotesize'--------------------------------------------
 aov.meat1 <- aov(Wbscore ~ tenderizer * time + carcass +
                  Error(roast), data=meatData)
 
+
 ## ----saov1,size='scriptsize'---------------------------------------------
 summary(aov.meat1)
+
 
 ## ----lme1,size='footnotesize'--------------------------------------------
 library(nlme)
@@ -21,8 +25,10 @@ lme.meat1 <- lme(Wbscore ~ tenderizer*time, data=meatData,
     random = ~1|carcass/roast)
 
 
+
 ## ----alme1,size='footnotesize'-------------------------------------------
 anova(lme.meat1)
+
 
 ## ----lmePVC,size='tiny'--------------------------------------------------
 lme.meatP <- lme(Wbscore ~ time, data=meatData, random = ~1|carcass/roast,
@@ -32,10 +38,12 @@ lme.meatV <- lme(Wbscore ~ time, data=meatData, random = ~1|carcass/roast,
 lme.meatC <- lme(Wbscore ~ time, data=meatData, random = ~1|carcass/roast,
                  correlation=corCompSymm(), subset=tenderizer=="C")
 
+
 ## ----almePVC,size='tiny'-------------------------------------------------
 anova(lme.meatP, Terms="time")
 anova(lme.meatV, Terms="time")
 anova(lme.meatC, Terms="time")
+
 
 ## ----tuk,size='tiny'-----------------------------------------------------
 # install.packages("multcomp")
@@ -45,6 +53,7 @@ summary(mcP)
 # confint(mcP)
 # plot(mcP)
 
-## ----mcp,include=FALSE,fig.width=8,fig.height=6--------------------------
+
+## ----mcp,include=TRUE,fig.width=8,fig.height=6,size='scriptsize',fig.show='hide'----
 plot(mcP) # Should do this for the other 2 levels of tenderizer too
 
