d39c7713d978b4b560074238114a5d3b9cb7f5f4
Richard Chandler <richard.chandlers@gmail.com>
2019-11-08 11:04:37 -0500

    Intro to modeling lecture

--- labs/repeated-measures/lab-repeated-measures.R
+++ labs/repeated-measures/lab-repeated-measures.R
@@ -4,8 +4,10 @@ plantData$plant <- factor(plantData$plant)
 plantData$week <- factor(plantData$week)
 str(plantData)
 
+
 ## ----headPlantData,size='scriptsize'-------------------------------------
 head(plantData, n=8)
+
 
 ## ----plotDataLines,echo=FALSE,fig.width=7,fig.height=5.5-----------------
 matplot(matrix(plantData$leaves, 5, 10), type="l",
@@ -15,31 +17,36 @@ matplot(matrix(plantData$leaves, 5, 10), type="l",
 legend(1,14,c("High fertilizer", "Low fertilizer"),
        lty=c(2,1), col=c(4,1), lwd=2)
 
+
 ## ----aov1,size='scriptsize'----------------------------------------------
 aov1 <- aov(leaves ~ fertilizer*week + Error(plant),
             data=plantData)
 
+
 ## ----aov1summary,size='scriptsize'---------------------------------------
 summary(aov1)
 
-## ----plantData2,size='footnotesize'--------------------------------------
+
+## ----plantData2,size='scriptsize'----------------------------------------
 plantData2 <- reshape(plantData, idvar="plant",
                       timevar="week", v.names="leaves",
                       direction="wide")
-
-## ----plantData2print,size='scriptsize'-----------------------------------
 plantData2
+
 
 ## ----manova1,size='scriptsize'-------------------------------------------
 manova1 <- manova(cbind(leaves.1, leaves.2, leaves.3,
                         leaves.4, leaves.5) ~ fertilizer,
                   data=plantData2)
 
+
 ## ----manova1anova,size='scriptsize'--------------------------------------
 anova(manova1, X=~1, test="Spherical")
 
+
 ## ----mauchly,size='scriptsize'-------------------------------------------
 mauchly.test(manova1, X=~1)
+
 
 ## ----compare1,size='tiny'------------------------------------------------
 summary(aov1)
@@ -47,11 +54,14 @@ summary(aov1)
 ## ----compare2,size='tiny'------------------------------------------------
 anova(manova1, X=~1, test="Spherical")
 
+
 ## ----wilks,size='footnotesize'-------------------------------------------
 anova(manova1, X=~1, test="Wilks")
 
+
 ## ----pillai,size='footnotesize'------------------------------------------
 anova(manova1, X=~1, test="Pillai")
+
 
 ## ----manova2,size='footnotesize'-----------------------------------------
 manova2 <- manova(
@@ -59,8 +69,10 @@ manova2 <- manova(
           leaves.4-leaves.3, leaves.5-leaves.4) ~
     fertilizer, data=plantData2)
 
+
 ## ----profile,size='scriptsize'-------------------------------------------
 summary.aov(manova2)
+
 
 ## ----plotit,size='footnotesize'------------------------------------------
 leavesMat <- plantData2[,3:7]
@@ -69,12 +81,14 @@ colnames(growthMat) <- paste("interval", 1:4, sep=".")
 (lowFertilizer <- colMeans(growthMat[1:5,]))
 (highFertilizer <- colMeans(growthMat[6:10,]))
 
+
 ## ----SEs,size='footnotesize'---------------------------------------------
 SE <- sqrt(diag(stats:::vcov.mlm(manova2)))
 SE <- SE[names(SE)==":(Intercept)"] # Only use "intercept" SEs
 unname(SE) ## Ignore the names
 
-## ----gr,fig.show='hide',fig.width=7,fig.height=5.5,size='small'----------
+
+## ----gr,fig.show='hide',fig.width=7,fig.height=5.5,size='footnotesize'----
 plot(1:4-0.05, lowFertilizer, type="b", xlim=c(0.9, 4.1),
      ylim=c(-1, 4), xaxp=c(1,4,3), cex.lab=1.5,
      xlab="Time interval", ylab="Growth rate (leaves/week)")
--- lectures/modeling-intro/lecture-modeling-intro.R
+++ lectures/modeling-intro/lecture-modeling-intro.R
@@ -2,15 +2,31 @@
 par(mai=c(0.8,0.8,0.1,0.1))
 plot(function(x) 20 + 0.5*x, 0, 10, ylab="y")
 
+
 ## ----linmod2,include=FALSE,fig.show='hide'-------------------------------
 par(mai=c(0.8,0.8,0.1,0.1))
 plot(function(x) 20 + 0.5*x - 0.3*x^2 , 0, 10, ylab="y")
 
+
+## ----linmod,include=FALSE------------------------------------------------
+set.seed(3400)  
+x <- runif(100, 0, 50)  
+y <- rnorm(100, 10 + 1*x, 5)
+plot(x, y)
+abline(lm(y~x))
+
+
+## ----linmod-out,size='tiny'----------------------------------------------
+lm(y~x)
+
+
 ## ----contr-trt,size='small'----------------------------------------------
 options(contrasts=c("contr.treatment","contr.poly"))
 
+
 ## ----contr-sum,size='small',eval=FALSE-----------------------------------
 ## options(contrasts=c("contr.sum","contr.poly"))
+
 
 ## ----cruz,echo=FALSE,results='hide'--------------------------------------
 library(unmarked)
@@ -37,8 +53,10 @@ summary(jayData$jays <- round(rnorm(nrow(jayData), jaymu, sqrt(sigmaSq))))
 summary(lm(jays ~ elevation + I(elevation^2) + chaparral + habitat + forest + seeds,
            jayData))
 
+
 ## ----cruz2-head----------------------------------------------------------
 head(cruz2)
+
 
 ## ----elev,fig.show='hide',fig.width=6,fig.height=4,echo=FALSE------------
 library(lattice)
@@ -48,12 +66,14 @@ levelplot(elevation ~ x + y, data=cruz2, aspect="iso",
           at=seq(10, 2300, length=100),
           xlab="Easting", ylab="Northing", main="Elevation")
 
+
 ## ----forest,fig.show='hide',fig.width=6,fig.height=4,echo=FALSE----------
 levelplot(forest ~ x + y, data=cruz, aspect="iso",
           xlab="",ylab="",main="Forest Cover",
           col.regions=rev(terrain.colors(100)),
           at=seq(0, 1, length=100),
           scales=list(draw=FALSE))
+
 
 ## ----plots,echo=FALSE,fig.show='hide',fig.width=6,fig.height=4-----------
 levelplot(chaparral ~ x + y, data=cruz2, aspect="iso",
@@ -68,12 +88,15 @@ levelplot(chaparral ~ x + y, data=cruz2, aspect="iso",
               }
           )
 
+
 ## ----jayData,size='scriptsize'-------------------------------------------
 head(jayData)
+
 
 ## ----fm1,size='scriptsize'-----------------------------------------------
 fm1 <- lm(jays ~ elevation, data=jayData)
 summary(fm1)
+
 
 ## ----pred1,echo=FALSE,fig.width=7,fig.height=5.5-------------------------
 nseq <- 20
@@ -85,9 +108,11 @@ lines(predData1$elevation, pred1$fit[,1])
 lines(predData1$elevation, pred1$fit[,2], lty=3)
 lines(predData1$elevation, pred1$fit[,3], lty=3)
 
+
 ## ----fm2,size='scriptsize'-----------------------------------------------
 fm2 <- lm(jays ~ elevation+forest, data=jayData)
 summary(fm2)
+
 
 ## ----pred2,echo=FALSE,fig.width=7,fig.height=6---------------------------
 forest.seq <- seq(0,1,length=nseq)
@@ -101,9 +126,11 @@ res <- persp(forest.seq, elev.seq, matrix(pred2$fit[,1],nseq),
 #points(trans3d(jayData$forest, jayData$elev, jayData$jays, pmat=res),
 #       col="red", cex=1.5, pch=16)
 
+
 ## ----fm3,size='scriptsize'-----------------------------------------------
 fm3 <- lm(jays ~ habitat, data=jayData)
 summary(fm3)
+
 
 ## ----pred3,echo=FALSE,fig.width=6,fig.height=6,fig.show='hide'-----------
 predData3 <- data.frame(habitat=levels(jayData$habitat))
@@ -114,9 +141,11 @@ xmid <- barplot(pred3$fit[,1], xlab="Habitat", names=predData3$habitat,
 arrows(xmid, pred3$fit[,1], xmid, pred3$fit[,1]+pred3$se, angle=90, length=0.05, code=3)
 box()
 
+
 ## ----fm4,size='scriptsize'-----------------------------------------------
 fm4 <- lm(jays ~ elevation+habitat, data=jayData)
 summary(fm4)
+
 
 ## ----pred4,echo=FALSE,fig.width=7,fig.height=5.5-------------------------
 predData4 <- data.frame(elevation=rep(elev.seq, 3),
@@ -129,9 +158,11 @@ lines(fit~elevation, data=pred4dat, subset=habitat=="Oak", col="brown", lwd=3)
 lines(fit~elevation, data=pred4dat, subset=habitat=="Pine", col="darkseagreen", lwd=3)
 legend(0, 47, c("Oak", "Pine", "Bare"), col=c("brown","darkseagreen", "tan"), lwd=3)
 
+
 ## ----fm5,size='tiny'-----------------------------------------------------
 fm5 <- lm(jays ~ elevation*habitat, data=jayData)
 summary(fm5)
+
 
 ## ----pred5,echo=FALSE,fig.width=7,fig.height=5.5-------------------------
 pred5 <- predict(fm5, newdata=predData4, interval="confidence", se.fit=TRUE)
@@ -142,9 +173,11 @@ lines(fit~elevation, data=pred5dat, subset=habitat=="Oak", col="brown", lwd=3)
 lines(fit~elevation, data=pred5dat, subset=habitat=="Pine", col="darkseagreen", lwd=3)
 legend(0, 47, c("Oak", "Pine", "Bare"), col=c("brown","darkseagreen", "tan"), lwd=3)
 
+
 ## ----fm6,size='scriptsize'-----------------------------------------------
 fm6 <- lm(jays ~ elevation+I(elevation^2), data=jayData)
 summary(fm6)
+
 
 ## ----pred6,echo=FALSE,fig.width=7,fig.height=5.5-------------------------
 nseq <- 20
@@ -156,18 +189,22 @@ lines(predData6$elevation, pred6$fit[,1])
 lines(predData6$elevation, pred6$fit[,2], lty=3)
 lines(predData6$elevation, pred6$fit[,3], lty=3)
 
+
 ## ----fm7,size='tiny'-----------------------------------------------------
 fm7 <- lm(jays ~ habitat * forest + elevation +
           I(elevation^2), data=jayData)
 summary(fm7)
 
+
 ## ----E7------------------------------------------------------------------
 E7 <- predict(fm7, type="response", newdata=cruz2,
               interval="confidence")
 
+
 ## ----E72-----------------------------------------------------------------
 E7 <- cbind(cruz2[,c("x","y")], E7)
 head(E7)
+
 
 ## ----Ejay,fig.show='hide',fig.width=6,fig.height=4,echo=FALSE------------
 levelplot(fit ~ x + y, data=E7, aspect="iso",
@@ -176,17 +213,20 @@ levelplot(fit ~ x + y, data=E7, aspect="iso",
           at=22:55, colorkey=list(space="bottom"),
           scales=list(draw=FALSE))
 
+
 ## ----Ljay,fig.show='hide',fig.width=6,fig.height=4,echo=FALSE------------
 levelplot(lwr ~ x + y, data=E7, aspect="iso",
           xlab="", ylab="", main="Lower CI",
           at=25:55, colorkey=list(space="bottom"),
           scales=list(draw=FALSE))
 
+
 ## ----Ujay,fig.show='hide',fig.width=6,fig.height=4,echo=FALSE------------
 levelplot(upr ~ x + y, data=E7, aspect="iso",
           xlab="", ylab="", main="Upper CI",
           at=25:55, colorkey=list(space="bottom"),
           scales=list(draw=FALSE))
+
 
 ## ----future1,echo=FALSE--------------------------------------------------
 future1 <- cruz2
@@ -200,6 +240,7 @@ levelplot(fit ~ x + y, data=future.pred1, aspect="iso",
           xlab="", ylab="", main="Expected values",
           at=25:55, colorkey=list(space="bottom"),
           scales=list(draw=FALSE))
+
 
 ## ----future2,echo=FALSE--------------------------------------------------
 future2 <- cruz2
@@ -219,31 +260,39 @@ levelplot(fit ~ x + y, data=future.pred2, aspect="iso",
           at=25:55, colorkey=list(space="bottom"),
           scales=list(draw=FALSE))
 
+
 ## ----fm1-2,size='scriptsize',eval=FALSE----------------------------------
 ## fm1 <- lm(jays ~ elevation, data=jayData)
+
 
 ## ----X1,size='scriptsize'------------------------------------------------
 X1 <- model.matrix(fm1)
 head(X1, n=5) # First 5 rows of design matrix
 
+
 ## ----beta-hat,size='scriptsize'------------------------------------------
-beta.hat <- coef(fm1) # Estimates of beta0 and beta1
-beta.hat
+beta.hat1 <- coef(fm1) # Estimates of beta0 and beta1
+beta.hat1
+
 
 ## ----Ey1,size='footnotesize'---------------------------------------------
-Ey1 <- X1 %*% beta.hat # Expected number of jays at each site
+Ey1 <- X1 %*% beta.hat1 # Expected number of jays at each site
 head(Ey1, 5)
+
 
 ## ----fm4-2,size='scriptsize',eval=FALSE----------------------------------
 ## fm4 <- lm(jays ~ elevation + habitat, data=jayData)
+
 
 ## ----X4,size='scriptsize'------------------------------------------------
 X4 <- model.matrix(fm4)
 head(X4, n=5) # First 5 rows of design matrix
 
+
 ## ----beta-hat4,size='scriptsize'-----------------------------------------
 beta.hat4 <- coef(fm4) # Estimates of beta0 and beta1
 beta.hat4
+
 
 ## ----Ey4,size='footnotesize'---------------------------------------------
 Ey4 <- X4 %*% beta.hat4 # Expected number of jays at each site
